60cfb5b07ad6b5e791c169ee48158f7319288379
Carlos Kruger <carloskr@yahoo.com>
2020-10-21 11:54:43 -0400

    first try

--- src/App.js
+++ src/App.js
@@ -1,1 +1,27 @@
+import React, { Component } from 'react'
+import './App.css';
+import List from './components/List'
 
+class App extends Component{
+constructor(){
+    super()
+    this.state = {
+
+    }
+}
+
+    render(){
+return(
+<div className="App">
+    <h1 className="movie-list-title">The Most Amazing Movie List</h1>
+    
+    <List/>
+
+</div>
+
+)
+    }
+
+}
+
+export default App;
